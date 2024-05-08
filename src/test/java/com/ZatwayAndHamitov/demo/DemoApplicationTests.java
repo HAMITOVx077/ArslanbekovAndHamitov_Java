7fccdf46b59419e3fb49689d8bee82cbdb630b7f
Tushev-Dmitriy <110030309+Tushev-Dmitriy@users.noreply.github.com>
2024-05-08 08:36:19 +0300

    Initial commit

---
+++ src/test/java/com/ZatwayAndHamitov/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ZatwayAndHamitov.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
